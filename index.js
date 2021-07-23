1c13d39c0f522fd4daaf14cc49a581ac0389d94c
allyboy08 <alexsassman65@gmail.com>
2021-07-23 16:00:33 +0200

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.end('welcome to node');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`server running at ${hostname}:${port}`);
+});
